828507d63d7731547dabe3f4b24afdee616f776d
Oleksandr Velchenko <oleksandr_velchenko@epam.com>
2019-02-28 13:56:31 +0200

    Add Iterator

---
+++ src/main/java/com/gof/behavioral/iterator/Client.java
@@ -0,0 +1,4 @@
+package com.gof.behavioral.iterator;
+
+public class Client {
+}
---
+++ src/main/java/com/gof/behavioral/iterator/CustomArrayIterator.java
@@ -0,0 +1,6 @@
+package com.gof.behavioral.iterator;
+
+public class CustomArrayIterator implements CustomIterator {
+    public CustomArrayIterator(int[] arr, int leng) {
+    }
+}
